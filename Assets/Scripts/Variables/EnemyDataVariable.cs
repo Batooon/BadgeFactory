73d4a07e48bbcb3b8794c1e84ebe9ff021ccf3fa
Anton <rozum.ant@gmail.com>
2020-02-03 20:32:19 +0200

    Added scriptable object EnemyDataVariable that contains next enemy data

---
+++ Assets/Scripts/Variables/EnemyDataVariable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class EnemyDataVariable : ScriptableObject
+{
+#if UNITY_EDITOR
+    [Multiline]
+    public string DeveloperDescription = "";
+#endif
+
+    public EnemyData EnemyDataVar;
+
+    public void SetValue(EnemyData data)
+    {
+        EnemyDataVar = data;
+    }
+
+    public static implicit operator EnemyData(EnemyDataVariable enemyData)
+    {
+        return enemyData.EnemyDataVar;
+    }
+}
