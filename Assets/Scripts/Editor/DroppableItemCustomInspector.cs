30561cf89e7f3bf5604e2920aa467bdaa0ba7382
Anton <rozum.ant@gmail.com>
2020-05-14 11:26:42 +0300

    Created Custom Inspector and Editor for Droppable items
    Also, I created a scriptable object and added it to DroppableItemsEditor folder

---
+++ Assets/Scripts/Editor/DroppableItemCustomInspector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ItemDataEditorObject))]
+public class DroppableItemCustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if(GUILayout.Button("Open Droppable items Editor"))
+        {
+            DroppableItemEditor.Open((ItemDataEditorObject)target);
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/DroppableItemEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class DroppableItemEditor : ExtendedEditorWindow
+{
+    public static void Open(ItemDataEditorObject itemData)
+    {
+        DroppableItemEditor window=GetWindow<DroppableItemEditor>("Droppable Item editor");
+        window.serializedObject=new SerializedObject(itemData);
+    }
+}
