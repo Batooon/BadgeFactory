c9517cae81626b09dfed2fe0c009f8cec0129bb2
Anton <rozum.ant@gmail.com>
2020-05-14 11:22:27 +0300

    Created Scriptable Object for Droppable Items
    this scriptable object will be used only in my custom editor

---
+++ Assets/Scripts/ScriptableObjects/ItemDataEditorObject.cs
@@ -0,0 +1,23 @@
+﻿using DroppableItems;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ItemEditorParams
+{
+    [SerializeReference]
+    [SelectImplementation(typeof(IDroppable))]
+    public IDroppable DroppableItem;
+    [Range(1,100)]
+    public int ChanceToSpawn;
+    public Sprite ItemSprite;
+}
+
+[CreateAssetMenu]
+public class ItemDataEditorObject : ScriptableObject
+{
+    [SerializeField]
+    public ItemEditorParams ItemEditor;
+}
