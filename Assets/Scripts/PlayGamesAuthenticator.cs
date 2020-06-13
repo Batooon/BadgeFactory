16da686cc961bd6a0dc2b7d46744605d51a95c6b
Anton <rozum.ant@gmail.com>
2020-06-14 00:35:04 +0300

    Created PlayGamesAuthenticator.cs to control player authentication in GP

---
+++ Assets/Scripts/PlayGamesAuthenticator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+using TMPro;
+
+public class PlayGamesAuthenticator : MonoBehaviour
+{
+    [SerializeField]
+    private string _signedInText;
+    [SerializeField]
+    private string _signedOutText;
+    [SerializeField]
+    private Toggle __googlePlayToggle;
+    [SerializeField]
+    private TextMeshProUGUI _googlePlayGamesText;
+
+    private void Awake()
+    {
+        UpdateText(PlayGamesPlatform.Instance.IsAuthenticated());
+
+        __googlePlayToggle.isOn = PlayGamesPlatform.Instance.IsAuthenticated();
+        __googlePlayToggle.onValueChanged.AddListener(OnTogglePressed);
+    }
+
+    private void OnTogglePressed(bool isAuthenticated)
+    {
+        if (isAuthenticated)
+            PlayGames.AuthenticateUser(UpdateText);
+        else
+            PlayGames.LogOut();
+    }
+
+    private void UpdateText(bool isAuthenticated)
+    {
+        if (isAuthenticated)
+        {
+            _googlePlayGamesText.text = _signedInText;
+        }
+        else
+        {
+            _googlePlayGamesText.text = _signedOutText;
+        }
+    }
+}
