72748e40312cdef6e63ecc294e8fb5b6eabf160f
Anton <rozum.ant@gmail.com>
2020-06-16 00:10:10 +0300

    rewrited CoinMover.cs

--- Assets/Scripts/CoinMover.cs
+++ Assets/Scripts/CoinMover.cs
@@ -14,9 +14,13 @@ public class CoinMover : MonoBehaviour, IItemTweener
         LeanTween.move(gameObject,
             new Vector2(Random.Range(-1.5f, 1.5f),
             Random.Range(-1.5f, 1.5f)),
-            .5f).setOnComplete(
-            () => LeanTween.move(gameObject, _destination, .5f).setOnComplete(_finished.Invoke));
-        
+            .5f).setOnComplete(Collect);
+    }
+
+    private void Collect()
+    {
+        LeanTween.move(gameObject, _destination, .5f).setOnComplete(_finished.Invoke);
+        LeanTween.scale(gameObject, Vector2.zero, .5f);
     }
 
     public void SetDestination(Vector2 destination)
