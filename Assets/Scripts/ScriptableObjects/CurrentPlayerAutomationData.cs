78a514c310590dca4c9f591f60b0c373cb913344
Anton <rozum.ant@gmail.com>
2020-06-30 22:34:16 +0300

    added serialized class to serialize CurrentPlayerAutomationData.cs

--- Assets/Scripts/ScriptableObjects/CurrentPlayerAutomationData.cs
+++ Assets/Scripts/ScriptableObjects/CurrentPlayerAutomationData.cs
@@ -1,11 +1,48 @@
-﻿[System.Serializable]
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SerializedCurrentPLayerAutomationData : ISerializationCallbackReceiver
+{
+    public bool CanUpgrade;
+
+    public CurrentPlayerAutomationData playerAutomationData = new CurrentPlayerAutomationData();
+
+    public void OnAfterDeserialize()
+    {
+        playerAutomationData.CanUpgrade = CanUpgrade;
+    }
+
+    public void OnBeforeSerialize()
+    {
+        CanUpgrade = playerAutomationData.CanUpgrade;
+    }
+}
+
+[Serializable]
 public class CurrentPlayerAutomationData
 {
+    public event Action UpgradeAvailabilityChanged;
     public int StartingCost;
     public int StartingDamage;
     public int Level;
     public int Cost;
     public int DamagePerSecond;
     public bool IsUnlocked;
-    public bool CanUpgrade;
+
+    public bool CanUpgrade
+    {
+        get => _canUpgrade;
+        set
+        {
+            if (_canUpgrade != value)
+            {
+                _canUpgrade = value;
+                UpgradeAvailabilityChanged?.Invoke();
+            }
+        }
+    }
+
+    private bool _canUpgrade;
+
 }
