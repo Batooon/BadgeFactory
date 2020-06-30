df160fe5ae55ec124e0d438ef971a523a21a7ebe
Anton <rozum.ant@gmail.com>
2020-06-30 22:33:21 +0300

    added class that indicates when player can upgrade smth

---
+++ Assets/Scripts/Automations/AutomationsUpgradeAvailableChecker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Automation;
+
+public class AutomationsUpgradeAvailableChecker : MonoBehaviour
+{
+    [SerializeField]
+    private Image _upgradeAvailabilityImage;
+
+    private AutomationDatabse _automationDatabase;
+
+    private void Awake()
+    {
+        _automationDatabase = AutomationDatabse.GetAutomationDatabase();
+    }
+
+    private void Start()
+    {
+        _automationDatabase.GetOverallAutomationsData().UpgradeAvailable += FetchUpgradeAvailability;
+        FetchUpgradeAvailability(_automationDatabase.CanUpgradeSomething());
+    }
+
+    private void FetchUpgradeAvailability(bool canUpgradeSomething)
+    {
+        _upgradeAvailabilityImage.gameObject.SetActive(canUpgradeSomething);
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            _automationDatabase.GetOverallAutomationsData().UpgradeAvailable -= FetchUpgradeAvailability;
+        }
+    }
+}
