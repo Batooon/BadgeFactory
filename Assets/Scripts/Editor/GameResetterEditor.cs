3ec1c56966163849b1a211ab35d0b8ea76169fb7
Anton <rozum.ant@gmail.com>
2020-07-29 14:36:15 +0300

    Created Editor scripts to easy control the game

---
+++ Assets/Scripts/Editor/GameResetterEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameResetter))]
+public class GameResetterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if(GUILayout.Button("Reset Game"))
+        {
+            GameResetter resetter = (GameResetter)target;
+            resetter.ResetGame();
+        }
+
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Scripts/Editor/GodModeEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GodMode))]
+public class GodModeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if(GUILayout.Button("Add some gold"))
+        {
+            GodMode godMode = (GodMode)target;
+            godMode.AddSomeGold();
+        }
+    }
+}
