7ed6d242f42071661c6caab1310d06b42a117f05
Anton <rozum.ant@gmail.com>
2020-07-29 14:27:49 +0300

    Added Vibration script

--- Assets/Scripts/Vibration.cs
+++ Assets/Scripts/Vibration.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Vibration : MonoBehaviour
 {
+    private SettingsData _settingsData;
+
+    public void Init(SettingsData settingsData)
+    {
+        _settingsData = settingsData;
+    }
+
     public void Vibrate()
     {
+        if (_settingsData.VibrationOff)
+            return;
+
         Handheld.Vibrate();
     }
 }
