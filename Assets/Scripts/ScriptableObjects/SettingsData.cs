f94da9724ae7c4600521827e93589faaf6d535fd
Anton <rozum.ant@gmail.com>
2020-07-29 14:25:17 +0300

    Created Settings Data
    To control and save settings state

---
+++ Assets/Scripts/ScriptableObjects/SettingsData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Settings Data")]
+public class SettingsData : ScriptableObject
+{
+    [SerializeField] private bool _vibrationOff;
+    [SerializeField] private bool _soundOff;
+    [SerializeField] private bool _musicOff;
+
+    public event Action<bool> VibrationChanged;
+    public event Action<bool> SoundChanged;
+    public event Action<bool> MusicChanged;
+
+    public bool VibrationOff
+    {
+        get => _vibrationOff;
+        set
+        {
+            _vibrationOff = value;
+            VibrationChanged?.Invoke(value);
+        }
+    }
+    public bool SoundOff
+    {
+        get => _soundOff;
+        set
+        {
+            _soundOff = value;
+            SoundChanged?.Invoke(value);
+        }
+    }
+    public bool MusicOff
+    {
+        get => _musicOff;
+        set
+        {
+            _musicOff = value;
+            MusicChanged?.Invoke(value);
+        }
+    }
+
+}
