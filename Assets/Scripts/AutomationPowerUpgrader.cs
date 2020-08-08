a2291815e542588010750a762a21b92d942b420e
Anton <rozum.ant@gmail.com>
2020-08-08 11:29:04 +0300

    Created implementations of IUpgrade interface

---
+++ Assets/Scripts/AutomationPowerUpgrader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AutomationPowerUpgrader : MonoBehaviour, IUpgrade
+{
+    public void Upgrade(AutomationsData automationsData, int percentage, int automationIndex)
+    {
+        long automationDamage = automationsData.Automations[automationIndex].CurrentDamage;
+
+        automationsData.AutomationsPower -= automationDamage;
+        automationsData.Automations[automationIndex].PowerUpPercentage += percentage;
+    }
+}
---
+++ Assets/Scripts/ClickPowerUpgrader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ClickPowerUpgrader : MonoBehaviour, IUpgrade
+{
+    public void Upgrade(AutomationsData automationsData, int percentage, int automationIndex)
+    {
+        automationsData.ClickPower += automationsData.ClickPower * (percentage / 100);
+        automationsData.Automations[automationIndex].PowerUpPercentage += percentage;
+    }
+}
